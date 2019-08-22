15c6a1ad7032c51bf3e588d0c30fbc058bdcff76
wuyoufa <442342472@qq.com>
2019-08-22 15:48:11 +0800

    first commit

---
+++ Assets/Scipts/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	[FormerlySerializedAs("guidObj")] [HideInInspector]
+	public GameObject guideObj; //引导物体
+
+	public Transform guideTrs; //存储下一步生成道路坐标以及位置信息
+	public GameObject roadTemplate; //道路模板
+
+	public int maxBuildRoundNum = 10; //一次最大生成数
+	public int turnSeedRange = 10;
+	public List<GameObject> roads; //保存现在能够进行还原的道路
+
+	//	[FormerlySerializedAs("isBuidDirRoad")]
+//	public bool isBuildDirRoad; //是否生成方向道路
+
+//	[HideInInspector]
+//	public int goldNumber; //吃到的金币数
+
+	int buildRoundNum = 0; //确定转向后的回合数
+//	int dirRoadType; //方向道路的类型
+//
+//	int dirRoadNumber; //方向道路的数量
+
+	private void Start()
+	{
+		guideObj = Instantiate(roadTemplate);
+		guideObj.transform.position = Vector3.zero;
+		guideObj.transform.rotation = Quaternion.identity;
+		guideObj.name = "Guide";
+
+		guideTrs = guideObj.transform; //上面实例化生成引导物体并存储下一步的道路信息
+
+		for (int i = 0; i < 20; i++) //预先生成20格道路作为跑道
+		{
+			var tmpRoad = Instantiate(roadTemplate, guideTrs.position, guideTrs.rotation);
+			guideTrs.position += guideTrs.forward; //每生成一个道路，引导物体的位置改变
+		}
+
+		for (int i = 0; i < 30; i++)
+		{
+			BuildRoad(); //先生成30个动画效果
+		}
+	}
+
+	// 回合数更新
+	private void resetBuildRoundNum()
+	{
+		buildRoundNum = maxBuildRoundNum; 
+	}
+
+	// 创建转向
+	private void BuildTurn()
+	{
+		int dictSeed = Random.Range(1, 2);
+		for (int i = 0; i < 3; i++) //先生成3个格子的道路，作为转向区
+		{
+			BuildStraight();
+
+		}
+
+		int rotationAngle = (dictSeed == 1) ? 90 : -90;
+		guideTrs.position -= guideTrs.forward * 2; //转向区域生成完成后，引导物体回退2格
+		guideTrs.Rotate(Vector3.up, rotationAngle); //转向
+		guideTrs.position += guideTrs.forward * 2; //转向后引导物体的forward轴改变，改变pos值，到达下一个道路的生成地点
+	}
+	
+	// 创建直路
+	private void BuildStraight()
+	{
+		var tmpRoad = Instantiate(roadTemplate, guideTrs.position, guideTrs.rotation);
+		PlayRoadAnimator(tmpRoad);
+		guideTrs.position += guideTrs.forward; //每生成一个道路，引导物体的位置改变
+		roads.Add(tmpRoad);
+	}
+	
+	/// <summary>
+	/// 生成道路的方法
+	/// </summary>
+	public void BuildRoad()
+	{
+		int turnSeed = Random.Range(1, turnSeedRange); //用于确定是否转向
+		if (turnSeed == 1 && buildRoundNum <= 0)
+		{
+			resetBuildRoundNum();
+			BuildTurn();
+		}
+		else
+		{
+			BuildStraight();
+		}
+
+		buildRoundNum--;
+	}
+
+	/// <summary>
+	/// 播放道路动画
+	/// </summary>
+	/// <param name="road"></param>
+	public void PlayRoadAnimator(GameObject road)
+	{
+		var tmpRoadController = road.GetComponent<RoadController>();
+		if (tmpRoadController == null)
+		{
+			return;
+		}
+
+		tmpRoadController.ChangeChildrens();
+	}
+}
---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Serialization;
+
+//引入DoTween插件
+/* 思考题
+ * 1：由于我们的地形有高有底，会导致我们的（playController.isGrounded）检测是否在地面有时失效。怎么解决呢？
+ * 2：玩家在进行下滑时，身形会能够通过比较低一点的地形，这一块这个Demo里面没有。那么我们如何实现这个功能呢？
+ */
+
+public class PlayerController : MonoBehaviour
+{
+	public float initPlayerSpeed = 3.0f;
+	public float speedUpCoefficient = 3.0f;
+	[FormerlySerializedAs("playAnimator")] [FormerlySerializedAs("playAnimtor")] 
+	public Animator playerAnimator;
+	
+	public float moveSpeed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+//	public float jumpPower; //玩家的跳跃高度
+//	[HideInInspector] public GameObject nowRoad; //现在玩家脚下的道路
+//	bool isTurnleftEnd = true; //左转向是否完成
+//	bool isTurnRightEnd = true; //右转向是否完成
+//	bool isJumpState; //现在是否是转向状态
+
+//	RuntimeAnimatorController nowController; //现在的动画控制器
+//	AnimationClip[] cilps;
+
+	CharacterController characterController;
+	Vector3 MoveIncrements;
+	[SerializeField] float transverseSpeed = 5.0f; //玩家横向的移动速度
+
+	void Start()
+	{		
+		characterController = GetComponent<CharacterController>();
+
+//		jumpPower = 3.0f;
+//		playController = GetComponent<CharacterController>();
+//		playAnimator = GetComponent<Animator>();
+//		nowController = playAnimator.runtimeAnimatorController;
+//		cilps = nowController.animationClips;
+//		for (int i = 0; i < cilps.Length; i++)
+//		{
+//			if (cilps[i].events.Length <= 0)
+//			{
+//				switch (cilps[i].name)
+//				{
+//					case "JUMP00":
+//						AnimationEvent endEvent = new AnimationEvent();
+//						endEvent.functionName = "JumpEnd";
+//						endEvent.time = cilps[i].length - (20.0f / 56.0f) * 1.83f;
+//						cilps[i].AddEvent(endEvent);
+//						AnimationEvent centerEvent = new AnimationEvent();
+//						centerEvent.functionName = "JumpCenter";
+//						centerEvent.time = cilps[i].length * 0.3f;
+//						cilps[i].AddEvent(centerEvent);
+//						break;
+//					case "SLIDE00":
+//						AnimationEvent slideEvent = new AnimationEvent();
+//						slideEvent.functionName = "SlideEnd";
+//						slideEvent.time = cilps[i].length - (15.0f / 42.0f) * 1.33f;
+//						cilps[i].AddEvent(slideEvent);
+//						break;
+//				}
+//			}
+//		}
+	}
+
+	void handleInput()
+	{
+//		if (Input.GetKeyDown(KeyCode.J) && isTurnleftEnd)
+//		{
+//			isTurnleftEnd = false; //更新转向状态
+//			transform.Rotate(Vector3.up, -90);
+//			Quaternion tmpQuaternion = transform.rotation; //计算转向后的四元数并保存
+//			transform.Rotate(Vector3.up, 90); //角度回滚
+//			Tween tween = transform.DORotateQuaternion(tmpQuaternion, 0.3f); //使用DoTween插件进行转向的平滑运动
+//			tween.OnComplete(() => isTurnleftEnd = true); //动画结束后转向状态更新
+//		}
+//
+//		if (Input.GetKeyDown(KeyCode.L) && isTurnRightEnd)
+//		{
+//			isTurnRightEnd = false;
+//			transform.Rotate(Vector3.up, 90);
+//			Quaternion tmpQuaternion = transform.rotation;
+//			transform.Rotate(Vector3.up, -90);
+//			Tween tween = transform.DORotateQuaternion(tmpQuaternion, 0.3f);
+//			tween.OnComplete(() => isTurnRightEnd = true);
+//		}
+//
+//		if (Input.GetKeyDown(KeyCode.Space) && playController.isGrounded)
+//		{
+//			isJumpState = true; //更新跳跃状态
+//			playAnimator.SetBool("IsJump", true); //播放跳跃动画
+//		}
+		
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+			SetPlayerMove();
+		}
+		
+//		if (Input.GetKeyUp(KeyCode.W))
+//		{
+//			SetPlayerIdle();
+//		}
+	}
+
+	void SetPlayerMove()
+	{
+		playerAnimator.SetBool("isMove", true);
+
+//		if (characterController.isGrounded) {
+//			MoveIncrements = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+//			MoveIncrements = transform.TransformDirection(MoveIncrements);
+//			MoveIncrements *= moveSpeed;
+//			if (Input.GetButton("Jump"))
+//				MoveIncrements.y = jumpSpeed;
+//		}
+//		MoveIncrements.y -= gravity * Time.deltaTime;
+		characterController.Move(transform.forward * Time.deltaTime);
+//		playerAnimator.SetFloat("moveSpeed", characterController.velocity.magnitude); //动画状态更新
+	}
+
+	void SetPlayerIdle()
+	{
+		playerAnimator.SetBool("isMove", false);
+	}
+	
+	void Update()
+	{
+//		moveSpeed += Time.deltaTime * 0.3f;
+//		float moveDir = Input.GetAxis("Horizontal");
+//		MoveIncrements = transform.forward * moveSpeed * Time.deltaTime;
+//		MoveIncrements += transform.right * transverseSpeed * Time.deltaTime * moveDir;
+//		if (isJumpState) //如果现在正在进行跳跃
+//		{
+//			MoveIncrements.y += jumpPower * Time.deltaTime;
+//		}
+//		else
+//		{
+//			MoveIncrements.y += playController.isGrounded ? 0f : -5.0f * Time.deltaTime * 1f; //更新重力
+//		}
+//
+//		playController.Move(MoveIncrements);
+//		playAnimator.SetFloat("MoveSpeed", playController.velocity.magnitude); //动画状态更新
+
+		handleInput();
+	}
+
+	/*private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if (hit.gameObject != nowRoad) //去重复避免删除错误
+		{
+			nowRoad = hit.gameObject;
+			Destroy(hit.gameObject, 1.0f);
+			GameManager.instance.BuidRoad(); //生成道路
+			GameManager.instance.CloseRoadAnimator();
+		}
+	}
+
+	public void JumpEnd()
+	{
+		playAnimator.SetBool("IsJump", false);
+	}
+
+	public void JumpCenter()
+	{
+		isJumpState = false;
+	}
+
+	public void SlideEnd()
+	{
+		playAnimator.SetBool("IsSlide", false);
+	}*/
+}
